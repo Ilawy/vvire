811044436d5a2846139e02954b3f9c9470b742e7
ilawy <mm.ilawy@gmail.com>
2024-09-29 01:23:59 +0300

    fix env variable name

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,6 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 const client = createClient({
   url: process.env.TURSO_URL!,
-  authToken: process.env.TURSO_TOKEN,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 export const db = drizzle(client);
